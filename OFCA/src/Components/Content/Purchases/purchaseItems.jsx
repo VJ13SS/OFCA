ca255510cc47bde52f01eca87481e43d3f715a59
VJ13SS <viswajith5749@gmail.com>
2025-01-31 15:29:05 +0530

    minor updates

--- OFCA/src/Components/Content/Purchases/purchaseItems.jsx
+++ OFCA/src/Components/Content/Purchases/purchaseItems.jsx
@@ -31,8 +31,8 @@ export default function PurchaseItems({
   };
 
   const [form, setForm] = useState({
-    totalPrice:totalPrice,
-    discount:discount,
+    totalPrice: totalPrice,
+    discount: discount,
     UserName: "",
     EmailAddress: "",
     CompanyName: "Not Available",
@@ -102,18 +102,55 @@ export default function PurchaseItems({
     }
   };
 
-  
+  console.log(cartItems)
+  const generateProductsTable = () => {
+    return `<table style = "
+    width: 90%;
+    border-collapse: collapse;">
+    <thead>
+      <tr>
+          <th style="text-align:center; border:1px solid black;padding:10px;">Level</th>
+          <th style="text-align:center; border:1px solid black;padding:10px;">Plan</th>
+          <th style="text-align:center; border:1px solid black;padding:10px;">Quantity</th>
+          <th style="text-align:center; border:1px solid black;padding:10px;">Price</th>
+      </tr>
+    </thead>
+    <tbody>
+      ${cartItems
+        .map((item) => {
+          return `
+          <tr>
+              <td style = "text-align:center;border:1px solid black;padding: 10px;">
+                <span>${item.level}</span>
+              </td>
+              <td style = "text-align:center;border:1px solid black;padding: 10px;">
+                <span>OFCA Certification Programme ${item.plan}</span>
+              </td>
+              <td style = "text-align:center;border:1px solid black;padding: 10px;">
+                <span>${item.quantity}</span>
+              </td>
+              <td style = "text-align:center;border:1px solid black;padding: 10px;">
+                <span>${item.amount * item.quantity}</span>
+              </td>
+          </tr>`;
+        })
+        .join("")}
+      </tbody>
+      </table>`;
+  };
 
-  const sendEmail =() => {
-    setForm({ ...form, products_table: generateProductsTable() });
-    emailjs.send("service", "template", form, "7xzu1_").then(
-      (response) => {
-        alert("Email send", response);
-      },
-      (error) => {
-        alert("Error", error);
-      }
-    );
+  const sendEmail = () => {
+    setForm({ ...form, products_table: generateProductsTable()});
+    emailjs
+      .send("service_44nolmr", "template_ud3pu5m", form, "7xzu1_S-S0TbFD6yt")
+      .then(
+        (response) => {
+          alert("Email send", response);
+        },
+        (error) => {
+          alert("Error", error);
+        }
+      );
   };
   return (
     <div className="purchase-items">
