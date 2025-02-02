1ab5730972b85e7edeec9fb6b00258e6990f1924
VJ13SS <viswajith5749@gmail.com>
2025-02-02 21:40:14 +0530

    updated the footer by adding the respective links

--- OFCA/src/Components/Footer/Footer.jsx
+++ OFCA/src/Components/Footer/Footer.jsx
@@ -1,5 +1,11 @@
 import "./Footer.css";
-import {FaEnvelope,FaFacebook,FaInstagram,FaLinkedin,FaYoutube} from 'react-icons/fa'
+import {
+  FaEnvelope,
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaYoutube,
+} from "react-icons/fa";
 //IG: https://www.instagram.com/ofcafitness?igsh=d3dtY2swbHJ5M20=
 //FB: https://www.facebook.com/share/15f2XhE4Re/
 //LinkedIn: https://www.linkedin.com/in/patrick-a-lyons?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app
@@ -18,16 +24,45 @@ export default function Footer() {
         <span className="email">plyonsfit@gmail.com</span>
       </div>
       <div className="footer-section-3">
-    
         <div className="links">
-          
-          <span className="icon" style={{color:'blue',marginLeft:'10px'}}><FaFacebook /></span>
-          <span className="icon" style={{color:'rgb(252,3,115'}}><FaInstagram /></span>
-          <span className="icon" style={{color:'red'}}><FaYoutube /></span>
-          <span className="icon" style={{color:'rgb(3,119,252'}}><FaLinkedin /></span>
+          <span className="icon">
+            <a
+              href="https://www.facebook.com/share/15f2XhE4Re/"
+              style={{ color: "blue" }}
+            >
+              <FaFacebook />
+            </a>
+          </span>
+          <span className="icon">
+            <a
+              href="https://www.instagram.com/ofcafitness?igsh=d3dtY2swbHJ5M20="
+              style={{ color: "rgb(252,3,115" }}
+            >
+              <FaInstagram />
+            </a>
+          </span>
+          <span className="icon">
+            <a
+              href="https://youtube.com/@patricklyonsofficial?si=5Qh4_bhnqPcpPn5D"
+              style={{ color: "red" }}
+            >
+              <FaYoutube />
+            </a>
+          </span>
+          <span className="icon">
+            <a
+              href="https://www.linkedin.com/in/patrick-a-lyons?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app
+"
+              style={{ color: "rgb(3,119,252" }}
+            >
+              <FaLinkedin />
+            </a>
+          </span>
         </div>
       </div>
-      <span style={{color:'gray',fontSize:'10px'}}>Created by VJ 13 SS</span>
+      <span style={{ color: "gray", fontSize: "10px" }}>
+        Created by VJ 13 SS
+      </span>
     </div>
   );
 }
